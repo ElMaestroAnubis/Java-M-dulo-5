b55adac7dd6c4c80c6956db242d3955b268f8aa7
Alfonso Lara <orozcoenrique159@gmail.com>
2025-08-13 21:23:55 -0500

    servicio agregado

--- Clase3/clase3/src/main/java/com/devsenior/clase3/Controller/MaterialController.java
+++ Clase3/clase3/src/main/java/com/devsenior/clase3/Controller/MaterialController.java
@@ -3,10 +3,51 @@ package com.devsenior.clase3.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsenior.clase3.Model.Material;
+import com.devsenior.clase3.Service.IMaterialService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 @RestController
 @RequestMapping("/materiales")
 public class MaterialController {
-    // GET /materiales: obtener todos los materiales
-    // GET /materiales/{id}: obtener un material por ID
+    
 
+
+    @Autowired
+    private IMaterialService service;
+
+    @PostMapping
+    public Material guardarMaterial(@RequestBody Material material) {
+        return service.guardarMaterial(material);
+    }
+    
+    @GetMapping
+    public List<Material> listarMateriales() {
+        return service.listarMateriales();
+    }
+
+    @GetMapping("/{id}") // ruta o path: /materiales/1
+    public Material buscarMaterialPorId(@PathVariable Long id) {
+        return service.buscarMaterialPorId(id);
+    }
+   
+    @PutMapping("/{id}/cantidad/{cantidad}") // ruta o path: /materiales/1/cantidad/10
+    public Material actualizarCantidad(@PathVariable Long id, @PathVariable int cantidad) {
+        return service.actualizarCantidad(id, cantidad);
+    }
+
+    @DeleteMapping("/{id}") // ruta o path: /materiales/1
+    public void eliminarMaterial(@PathVariable Long id) {
+        service.eliminarMaterial(id);
+    }
 }
--- Clase3/clase3/src/main/java/com/devsenior/clase3/Service/IMaterialService.java
+++ Clase3/clase3/src/main/java/com/devsenior/clase3/Service/IMaterialService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.devsenior.clase3.Model.Material;
 
 public interface IMaterialService {
-    Material guardarMaterial(Material material);
+    Material guardarMaterial(Material material); //
     Material buscarMaterialPorId(Long id);
     void eliminarMaterial(Long id);
     Material actualizarCantidad(Long id, int cantidad);
